0dd7f658b63d830b17a898e85f59ecd418bbd9eb
Parkchangsoo202055084 <3617kyen@naver.com>
2026-04-06 18:36:52 +0900

    changsootest2 CORSMiddleware 추가

--- backend/main.py
+++ backend/main.py
@@ -47,7 +47,8 @@ app.add_middleware(
         "http://localhost:3000",
         "https://cxnexus.ai",
         "https://www.cxnexus.ai",
-        "https://emotion-ai-jkax-wqsd.vercel.app"
+        "https://emotion-ai-jkax-wqsd.vercel.app",
+        "https://emotion-ai-jkax-wqsd-git-changsootest2-dlswn422s-projects.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
